d155433d26ff8b6e68d111f4a3feec4c1d18dea6
louie <louiecasula@gmail.com>
2023-12-09 19:48:15 -0500

    Got rid of junk HMTL, began modifying TS

--- index.ts
+++ index.ts
@@ -3,6 +3,7 @@ import './style.css';
 
 
 const form: HTMLFormElement = document.querySelector('#defineform');
+const output: HTMLDivElement = document.querySelector('#output');
 
 
 form.onsubmit = () => {
@@ -11,5 +12,17 @@ form.onsubmit = () => {
   console.log(formData);
   const text = formData.get('defineword') as string;
   console.log(text);
-  return false; // prevent reload
+  // return false; // prevent reload
+  try{
+    const response = await fetch('https://api.dictionaryapi.dev/api/v2/entries/en/${word}')
+    const data = await response.json();
+
+    output.innerHTML = '<p>Definition: ${data.definition} </p>';
+  } catch(error){
+    console.error('Error fetching data:', error);
+    output.innerHTML = 'Word not found'
+  }
 };
+
+
+
